72c5dc7d8170c4e19c158cdf9957e26174e2fe48
Kostis <polu_kostas@yahoo.gr>
2022-10-23 12:29:46 +0300

    get update delete users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,38 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Delete, Patch, Param, Query } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
 
-    constructor( private userService: UsersService) {}
+    constructor( private usersService: UsersService) {}
+
+    //Parsed query param is always a string
+    //string id parseint(id) after query - Cast to int in service
 
     @Post('/signup')
     createUser(@Body() body: CreateUserDto) {
-        this.userService.create( body.email, body.password)
+        this.usersService.create( body.email, body.password)
+    }
+
+    @Get('/:id')
+    findUser(@Param('id') id: string) {
+        return this.usersService.findOne(parseInt(id));
+    }
+
+    @Get()
+    findAllUsersByEmail(@Query('email') email: string) {
+        return this.usersService.findByEmail(email);
+    }
+
+    @Delete('/:id')
+    removeUser(@Param('id') id: string) {
+        return this.usersService.remove(parseInt(id));
+    }
+
+    @Patch('/:id')
+    updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+        return this.usersService.update(parseInt(id), body);
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -11,8 +11,40 @@ export class UsersService {
     }
 
     create(email: string, password: string){
-        const user = this.repository.create({ email, password});
+        const user = this.repository.create({ email, password });
 
         return this.repository.save(user);
+    }
+
+    findOne(id: number) {
+        return this.repository.findOneBy( { id });
+    }
+
+    findByEmail(email: string) {
+        return this.repository.findBy( { email });
+    }
+
+    async update(id: number, attrs: Partial<User>) {//Pass attrs ( partial obj) to search and update partially data props
+        const userObj = await this.findOne(id);
+
+        if ( !userObj ) {
+            console.log(`No use found by ID ${id}`);
+        }
+
+        //Assign attrs to target obj and save
+        Object.assign(userObj, attrs);
+
+        return this.repository.save(userObj);
+    }
+
+    async remove(id: number) {
+
+        const userObj = await this.findOne(id);
+
+        if ( !userObj ) {
+            console.log(`No use found by ID ${id}`);
+        }
+
+        return this.repository.remove(userObj);
     }
 }
